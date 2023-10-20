d0a5e38f0f441d9994c67f91392d3f367ba710cb
i1tao <lumosmagicb00m@gmail.com>
2023-10-20 21:42:34 +0800

    refactor(Ui):Table Selectable

--- src/WinsecArkGui/window/process.hpp
+++ src/WinsecArkGui/window/process.hpp
@@ -9,22 +9,21 @@ namespace App
     {
         namespace ProcessWindow
         {
+
+
             static bool isOpen = true ;
+            static bool selectedRow[10] = { false };
             void Draw()
             {
                 if (isOpen)
                 {
                     ImGui::Begin(u8"进程(Process)", &isOpen, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoTitleBar);
-                    static ImGuiTableFlags flags = ImGuiTableFlags_ScrollX
-                        | ImGuiTableFlags_ScrollY
-                        | ImGuiTableFlags_RowBg
-                        | ImGuiTableFlags_BordersOuter
-                        | ImGuiTableFlags_BordersV
-                        | ImGuiTableFlags_Resizable
-                        | ImGuiTableFlags_Reorderable
-                        | ImGuiTableFlags_Hideable
-                        | ImGuiTableFlags_Sortable
-                    |ImGuiTableFlags_SortMulti;
+                    static ImGuiTableFlags flags =
+                        ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable
+                        | ImGuiTableFlags_Sortable | ImGuiTableFlags_SortMulti
+                        | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders | ImGuiTableFlags_NoBordersInBody
+                        | ImGuiTableFlags_ScrollX | ImGuiTableFlags_ScrollY
+                        | ImGuiTableFlags_SizingFixedFit;
 
                     ImVec2 outer_size = ImVec2(0.0f, ImGui::GetTextLineHeightWithSpacing() * 8);
                     if (ImGui::BeginTable("table_process", 5, flags, outer_size))
@@ -37,35 +36,48 @@ namespace App
                         ImGui::TableSetupColumn(u8"Description", ImGuiTableColumnFlags_None);
                         ImGui::TableHeadersRow();
 
+                        static int selectedRow = -1;
                         // Demonstrate using clipper for large vertical lists
-                        ImGuiListClipper clipper;
-                        clipper.Begin(5);
-                        while (clipper.Step())
+                        for (int i = 0; i < 10; i++)
                         {
-                            for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; row++)
-                            {
-                                ImGui::TableNextRow();
-                                for (int column = 0; column < 3; column++)
-                                {
-                                    ImGui::TableSetColumnIndex(column);
-                                    ImGui::Text("%d Hello %d,%d          ", row, column, row);
+                            ImGui::TableNextRow();
 
-                                    if (ImGui::IsItemHovered()) {
-                                        // 在悬停时可以高亮当前行
-                                        //ImGui::TableSetBgColor(ImGuiTableBgTarget_RowBg1, ImGui::GetColorU32(ImGuiCol_TableRowBgHovered));
-                                    }
+                            ImGui::TableSetColumnIndex(0);
 
-                                    // 使用 IsItemClicked() 检测项目是否被点击
-                                    if (ImGui::IsItemClicked()) {
-                                        // 在这里可以处理选中该项目的逻辑，例如，记录选中的项目或执行某些操作
-                                        //selected_item = i;
-                                    }
-                                }
-                            }
+                            ImGui::Selectable("Index", selectedRow == i, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowOverlap, ImVec2(0, 0));
+                            ImGui::TableNextColumn();
+                            ImGui::Text("Data %d", i + 1);
+
+                            ImGui::TableNextColumn();
+                            ImGui::Text("Value %d", i + 1);
+
+                            ImGui::TableNextColumn();
+                            ImGui::Text("Item %d", i + 1);
+
+                            ImGui::TableNextColumn();
+                            ImGui::Text("Amount %d", i + 1);
+
+                            // 监听行是否被点击并选中
+
+                            //if (ImGui::IsItemClicked(0))
+                            //{
+                            //    selectedRow = i;
+                            //}
+                            ////if (ImGui::Selectable("label", selectedRow, ImGuiSelectableFlags_SpanAllColumns | ImGuiSelectableFlags_AllowOverlap, ImVec2(0, 0)))
+                            ////{
+                            ////    selectedRow = i;
+                            ////}
+
+                            //if (selectedRow == i)
+                            //{
+                            //    ImGui::TableSetBgColor(ImGuiTableBgTarget_RowBg0, IM_COL32(0, 100, 0, 255));
+                            //}
                         }
+                        
+                        // 判断当前行是否被选中，若是，设置高亮颜色
+                        
                         ImGui::EndTable();
                     }
-
                     ImGui::End();
                 }
             }

c5849fa833323444da59424780c8699dd7d68179
i1tao <lumosmagicb00m@gmail.com>
2024-01-16 23:33:15 +0800

    refactor(GUI):about dlg

--- src/WinsecArkGui/dialog/about.hpp
+++ src/WinsecArkGui/dialog/about.hpp
@@ -22,14 +22,14 @@ namespace App
                     }
 
                     ImGui::Text(u8"Winsec Anti-Rookit Tool");
-                    ImGui::Text(u8"一个简单、免费的开源 Anti-Rookit 工具");
+                    ImGui::Text(g_Langueage ? u8"一个简单、免费的开源 Anti-Rookit 工具":u8"A simple & free open source ARK");
                     ImGui::Text(u8"Author: i1tao");
                     ImGui::Text(u8"Github: https://github.com/i1tao/winsec-ark");
                     ImGui::Separator();
                     ImGui::Text(u8"Thanks for:");
                     ImGui::Text(u8"           IMGUI");
                     ImGui::Separator();
-                    ImGui::Text(u8"Copyright © 2023 i1tao. All rights reserved.");
+                    ImGui::Text(u8"Copyright © 2024 i1tao. All rights reserved.");
 
                     ImGui::End();
                 }
